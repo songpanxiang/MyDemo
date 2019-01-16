191fb94d8f2a723e8cdf280c1fd5aea6290c22b8
songpanxiang <lanerxiangxiang@163.com>
2019-01-16 15:57:48 +0800

    加入URL

--- app/src/main/java/com/example/tron/tempdemo/MainActivity.java
+++ app/src/main/java/com/example/tron/tempdemo/MainActivity.java
@@ -5,17 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.tron.tempdemo.view.MyRatingBar;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private MyRatingBar appRatingbar;
     private TextView tv;
     private RecyclerView rllist;
+    private Button btGet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         appRatingbar.setClickable(false);
 
         rllist = findViewById(R.id.rllist);
-       // appRatingbar.setStar(0);
+        // appRatingbar.setStar(0);
 
         tv = findViewById(R.id.tv);
         tv.setMovementMethod(ScrollingMovementMethod.getInstance());
@@ -34,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 v.getParent().requestDisallowInterceptTouchEvent(true);
-                switch (event.getAction()){
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_UP:
                         v.getParent().requestDisallowInterceptTouchEvent(false);
                         break;
@@ -42,6 +52,47 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        btGet = findViewById(R.id.bt_get);
+        btGet.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_get:
+                getData("https://www.baidu.com");
+                break;
+        }
+    }
+
+
+    private void getData(final String url) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    StringBuilder jsonData = new StringBuilder();
+//
+                    URL mUrl = new URL(url);
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(mUrl.openStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        jsonData.append(line);
+                    }
+                    reader.close();
+
+                    Log.i("xiangxiang", "获取的网络数据： " + jsonData.toString());
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
 
     }
 }
